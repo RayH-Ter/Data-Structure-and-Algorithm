491a7d028a567a91764ad1f97a1dfcfd8b0369b4
RaymondHo <2963165392@qq.com>
2022-09-09 11:02:36 +0800

    some tests on vector

--- Example/vector_test/test2.cpp
+++ Example/vector_test/test2.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 int main(int agrc, char* agrv[])
 {
+    // int b = 0;
+    // int& a = b;
+    // b = 1;
+    // std::cout << a << std::endl;
+    // return 0;
     int b = 0;
-    int& a = b;
-    b = 1;
-    std::cout << a << std::endl;
+    int* a = &b;
+    *a = 1;
+    std::cout << b << std::endl;
     return 0;
 }
